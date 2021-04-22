a4f6b3e812aacfe9323e02bd9fd03e6c677d5c29
Luan Alves <akaluan.la@gmail.com>
2021-04-22 20:20:31 -0300

    Componentes Content, Header e SideBar finalizados

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button'
+
+interface SidebarProps {
+  genres: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }[],
+  handleClickButton: Function,
+  selected: Function
+}
+
+export function SideBar({ genres, handleClickButton, selected }: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selected(genre.id)}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
